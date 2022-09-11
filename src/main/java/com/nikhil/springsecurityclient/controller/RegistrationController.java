e9dd6bc0491c43d43c9a02b7856d5f2ce98b65cd
Nikhil <nsoni19012001@gmail.com>
2022-09-11 17:16:28 +0530

    Added Reset Password

--- src/main/java/com/nikhil/springsecurityclient/controller/RegistrationController.java
+++ src/main/java/com/nikhil/springsecurityclient/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package com.nikhil.springsecurityclient.controller;
 import com.nikhil.springsecurityclient.entity.User;
 import com.nikhil.springsecurityclient.entity.VerificationToken;
 import com.nikhil.springsecurityclient.event.RegistrationCompleteEvent;
+import com.nikhil.springsecurityclient.model.PasswordModel;
 import com.nikhil.springsecurityclient.model.UserModel;
 import com.nikhil.springsecurityclient.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,8 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @Slf4j
@@ -42,6 +45,39 @@ public class RegistrationController {
         User user = verificationToken.getUser();
         resendVerificationTokenMail(user, applicationUrl(request), verificationToken);
         return "Verification Link sent";
+    }
+
+    @PostMapping("/resetPassword")
+    public String resetPassword(@RequestBody PasswordModel passwordModel, HttpServletRequest request) {
+        User user = userService.findUserByEmail(passwordModel.getEmail());
+        String url = "";
+        if(user != null) {
+            String token = UUID.randomUUID().toString();
+            userService.createPasswordResetTokenForUser(user, token);
+            url = passwordResetTokenMail(user, applicationUrl(request), token);
+        }
+        return url;
+    }
+
+    @PostMapping("/savePassword")
+    public String savePassword(@RequestParam("token") String token, @RequestBody PasswordModel passwordModel) {
+        String result = userService.validatePasswordResetToken(token);
+        if (!result.equalsIgnoreCase("valid")) return "Invalid Token";
+
+        Optional<User> user = userService.getUserByPasswordResetToken(token);
+        if(user.isPresent()) {
+            userService.changePassword(user.get(), passwordModel.getNewPassword());
+            return "Password Reset Successful";
+        } else{
+            return "Invalid Token";
+        }
+    }
+
+    private String passwordResetTokenMail(User user, String applicationUrl, String token) {
+        String url = applicationUrl + "/savePassword?token=" + token;
+        // sendVerificationEmail()
+        log.info("Click the link to reset your password : {}", url);
+        return url;
     }
 
     private void resendVerificationTokenMail(User user, String applicationUrl, VerificationToken verificationToken) {
--- src/main/java/com/nikhil/springsecurityclient/service/UserService.java
+++ src/main/java/com/nikhil/springsecurityclient/service/UserService.java
@@ -4,6 +4,8 @@ import com.nikhil.springsecurityclient.entity.User;
 import com.nikhil.springsecurityclient.entity.VerificationToken;
 import com.nikhil.springsecurityclient.model.UserModel;
 
+import java.util.Optional;
+
 public interface UserService {
     User registerUser(UserModel userModel);
 
@@ -12,4 +14,14 @@ public interface UserService {
     String validateVerificationToken(String token);
 
     VerificationToken generateNewVerificationToken(String oldToken);
+
+    User findUserByEmail(String email);
+
+    void createPasswordResetTokenForUser(User user, String token);
+
+    String validatePasswordResetToken(String token);
+
+    Optional<User> getUserByPasswordResetToken(String token);
+
+    void changePassword(User user, String newPassword);
 }
