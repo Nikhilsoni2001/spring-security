5982c296ea54056d6d5a44bf5ebf6a6884ac8b43
Nikhil <nsoni19012001@gmail.com>
2022-09-11 15:18:52 +0530

    Added Resend Verification Token

--- src/main/java/com/nikhil/springsecurityclient/controller/RegistrationController.java
+++ src/main/java/com/nikhil/springsecurityclient/controller/RegistrationController.java
@@ -1,9 +1,11 @@
 package com.nikhil.springsecurityclient.controller;
 
 import com.nikhil.springsecurityclient.entity.User;
+import com.nikhil.springsecurityclient.entity.VerificationToken;
 import com.nikhil.springsecurityclient.event.RegistrationCompleteEvent;
 import com.nikhil.springsecurityclient.model.UserModel;
 import com.nikhil.springsecurityclient.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@Slf4j
 public class RegistrationController {
 
     @Autowired
@@ -31,6 +34,20 @@ public class RegistrationController {
         String result = userService.validateVerificationToken(token);
         if (result.equalsIgnoreCase("valid")) return "User Verified Successfully";
         return "Bad User";
+    }
+
+    @GetMapping("/resendVerificationToken")
+    public String resendVerificationToken(@RequestParam("token") String oldToken, HttpServletRequest request) {
+        VerificationToken verificationToken = userService.generateNewVerificationToken(oldToken);
+        User user = verificationToken.getUser();
+        resendVerificationTokenMail(user, applicationUrl(request), verificationToken);
+        return "Verification Link sent";
+    }
+
+    private void resendVerificationTokenMail(User user, String applicationUrl, VerificationToken verificationToken) {
+        String url = applicationUrl + "/verifyRegistration?token=" + verificationToken.getToken();
+        // sendVerificationEmail()
+        log.info("Click the link to verify your account : {}", url);
     }
 
     private String applicationUrl(HttpServletRequest request) {
